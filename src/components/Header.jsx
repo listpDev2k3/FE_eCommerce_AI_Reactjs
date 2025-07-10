f361778f616e89ea8dbc153809ba708d13419610
Dang Nguyen <nguyendangnguyen1606@gmail.com>
2025-07-10 12:50:37 +0700

    Create Project web Fe_eCommerce_AI

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <div>
+            <h2>web demo</h2>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Header
